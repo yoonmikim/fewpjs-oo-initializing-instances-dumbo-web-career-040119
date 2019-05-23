39dedd7bfca705754de457b501c564147c41a2d9
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-23 20:43:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ class Lunch {
   
 }
 
-class Lunch {
+class Dinner {
   
   constructor(salad, soup, entree, dessert) {
     this.salad = salad;
     this.soup = soup;
     this.entree = entree;
-    this._dessert = dessert;
+    this.dessert = dessert;
   }
 }
